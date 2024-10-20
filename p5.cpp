be7981659bf98d9076e0daaf58761f4434b95d45
prasagit99 <saumya20011124@gmail.com>
2024-10-20 21:01:46 +0530

    Add files via upload

---
+++ p5.cpp
@@ -0,0 +1,12 @@
+# include <iostream>
+using namespace std;
+int main ()
+{
+    cout << "Enter your name " << endl;
+    string name;
+    getline (cin,name);
+    cout << "My name is " << name;
+
+    return 0;
+}
+
